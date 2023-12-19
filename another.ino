7cfd91984566dc5d3b6294333eb73ee5e4e87118
jess <130740598+jes109@users.noreply.github.com>
2023-12-19 11:19:58 +0800

    Update another.ino

--- another.ino
+++ another.ino
@@ -1,11 +1,10 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
 #include <SimpleDHT.h>
+#include <HTTPClient.h>
 #include <BluetoothSerial.h>
 BluetoothSerial SerialBT;
-const char ssid[] = "Letmeconnect";
-const char password[] = "morse11111";
-String url = "http://api.thingspeak.com/update?api_key=R1H2QFMF34U13LNI";
+const char ssid[] = "jessie";
+const char password[] = "chochorai";
+String url = "http://api.thingspeak.com/update?api_key=ZZF3VNRSZW5S7FH6";
 int pinDHT11 = 15;
 SimpleDHT11 dht11(pinDHT11);
 int a=14;
@@ -17,123 +16,16 @@ int f=13;
 int g=12;
 int dp=21;
 int BTN=18;
-int BUZZ=2;
+int BUZZ=25;
 int GREEN=17;
 int RED=16;
 int buttonstate = 0;
 int lastButtonstate = 0;
-void digital_0(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 1);
-  digitalWrite(c, 1);
-  digitalWrite(d, 1);
-  digitalWrite(e, 1);
-  digitalWrite(f, 1);
-  digitalWrite(g, 0);  
-  delay(1000);
-}
-void digital_1(void)
-{
-  digitalWrite(a, 0);
-  digitalWrite(b, 1);
-  digitalWrite(c, 1);
-  digitalWrite(d, 0);
-  digitalWrite(e, 0);
-  digitalWrite(f, 0);
-  digitalWrite(g, 0);
-  delay(1000);
-}
-void digital_2(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 1);
-  digitalWrite(c, 0);
-  digitalWrite(d, 1);
-  digitalWrite(e, 1);
-  digitalWrite(f, 0);
-  digitalWrite(g, 1);
-  delay(1000);
-}
-void digital_3(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 1);
-  digitalWrite(c, 1);
-  digitalWrite(d, 1);
-  digitalWrite(e, 0);
-  digitalWrite(f, 0);
-  digitalWrite(g, 1);
-  delay(1000);
-}
-void digital_4(void)
-{
-  digitalWrite(a, 0);
-  digitalWrite(b, 1);
-  digitalWrite(c, 1);
-  digitalWrite(d, 0);
-  digitalWrite(e, 0);
-  digitalWrite(f, 1);
-  digitalWrite(g, 1);
-  delay(1000);
-}
-void digital_5(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 0);
-  digitalWrite(c, 1);
-  digitalWrite(d, 1);
-  digitalWrite(e, 0);
-  digitalWrite(f, 1);
-  digitalWrite(g, 1);
-  delay(1000);
-}
-void digital_6(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 0);
-  digitalWrite(c, 1);
-  digitalWrite(d, 1);
-  digitalWrite(e, 1);
-  digitalWrite(f, 1);
-  digitalWrite(g, 1);
-  delay(1000);
-  }
-void digital_7(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 1);
-  digitalWrite(c, 1);
-  digitalWrite(d, 0);
-  digitalWrite(e, 0);
-  digitalWrite(f, 0);
-  digitalWrite(g, 0);
-  delay(1000);
-}
-void digital_8(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 1);
-  digitalWrite(c, 1);
-  digitalWrite(d, 1);
-  digitalWrite(e, 1);
-  digitalWrite(f, 1);
-  digitalWrite(g, 1);
-  delay(1000);
-}
-void digital_9(void)
-{
-  digitalWrite(a, 1);
-  digitalWrite(b, 1);
-  digitalWrite(c, 1);
-  digitalWrite(d, 0);
-  digitalWrite(e, 0);
-  digitalWrite(f, 1);
-  digitalWrite(g, 1);
-  delay(1000);
-}
 void setup() {
   pinMode(BTN,INPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BUZZ, OUTPUT);
   pinMode(a, OUTPUT);  
   pinMode(b, OUTPUT);
   pinMode(c, OUTPUT);
@@ -142,9 +34,15 @@ void setup() {
   pinMode(f, OUTPUT);
   pinMode(g, OUTPUT);
   pinMode(dp, OUTPUT);
-  digitalWrite(dp, 0);
-  Serial.begin(115200);
-  Serial.print("開始連線到無線網路SSID:");
+  digitalWrite(dp, 1);
+  digitalWrite(a, 1);
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+  Serial.print(F("開始連線到無線網路SSID:"));
   Serial.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -152,26 +50,37 @@ void setup() {
     Serial.print(".");
     delay(1000);
   }
-  Serial.println("連線完成");
+  Serial.println(F("連線完成"));
+  SerialBT.begin(F("ESP32_BT"));
   Serial.begin(115200);
-  Serial.println("hello");
-  SerialBT.begin("ESP32_Morse");
 }
 void loop() {
   buttonstate = digitalRead(BTN);
   if(buttonstate==0&&lastButtonstate==1){
-    Serial.println("==========");
+    Serial.println(F("=========="));
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("溫度計讀取失敗，錯誤碼="); Serial.println(err); delay(1000);
+    Serial.print(F("溫度計讀取失敗，錯誤碼=")); Serial.println(err); delay(1000);
     return;
   }
-  Serial.print("溫度計讀取成功: ");
-  Serial.print((int)temperature); Serial.print(" *C, ");
-  Serial.print((int)humidity); Serial.println("%H");
-  int ans=(int)temperature%10;
+  Serial.print(F("溫度計讀取成功: "));
+  Serial.print((int)temperature); Serial.print(F(" *C, "));
+  Serial.print((int)humidity); Serial.println(F("%H"));
+  Serial.println(F("啟動網頁連線"));
+  HTTPClient http;
+  String url1 = url + "&field1=" + (int)temperature + "&field2=" + (int)humidity;
+  http.begin(url1);
+  int httpCode = http.GET();
+  if (httpCode == HTTP_CODE_OK){
+    String payload = http.getString();
+    Serial.print(F("網頁內容="));
+    Serial.println(payload);
+  } else {
+    Serial.println(F("網路傳送失敗"));
+  }
+  int ans=(int)temperature%10;  
   char n;
   if(ans==0){n1();n='0';}
   else if(ans==1){n1();n='1';}
@@ -213,22 +122,108 @@ void loop() {
   else if(ans==7){digital_7();}
   else if(ans==8){digital_8();}
   else if(ans==9){digital_9();}
-  Serial.println("啟動網頁連線");
-  HTTPClient http;
-  String url1 = url + "&field1=" + (int)temperature + "&field2=" + (int)humidity;
-  http.begin(url1);
-  int httpCode = http.GET();
-  if (httpCode == HTTP_CODE_OK){
-    String payload = http.getString();
-    Serial.print("網頁內容=");
-    Serial.println(payload);
-  } else {
-    Serial.println("網路傳送失敗");
-  }
-  http.end();
-  delay(5000);
   }
   lastButtonstate = buttonstate;
+}
+void digital_0(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 1);  
+}
+void digital_1(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+}
+void digital_2(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 1);
+  digitalWrite(g, 0);
+}
+void digital_3(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 0);
+}
+void digital_4(void)
+{
+  digitalWrite(a, 1);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+}
+void digital_5(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+  digitalWrite(e, 1);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+}
+void digital_6(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+  }
+void digital_7(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+}
+void digital_8(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+}
+void digital_9(void)
+{
+  digitalWrite(a, 0);
+  digitalWrite(b, 0);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
 }
 void n1 () {dot();dash();dash();dash();dash();}
 void n2 () {dot();dot();dash();dash();dash();}
@@ -240,5 +235,5 @@ void n7 () {dash();dash();dot();dot();dot();}
 void n8 () {dash();dash();dash();dot();dot();}
 void n9 () {dash();dash();dash();dash();dot();}
 void n0 () {dash();dash();dash();dash();dash();}
-void dot () {digitalWrite(BUZZ,HIGH); tone(BUZZ, 500); delay (100); digitalWrite(BUZZ,LOW); noTone(BUZZ); delay (200);}//改
-void dash () {digitalWrite(BUZZ,HIGH); tone(BUZZ, 500); delay (300); digitalWrite(BUZZ,LOW); noTone(BUZZ); delay (200);}//改
+void dot () {digitalWrite(BUZZ,HIGH); delay (100); digitalWrite(BUZZ,LOW); delay (200);}
+void dash () {digitalWrite(BUZZ,HIGH); delay (300); digitalWrite(BUZZ,LOW); delay (200);}
